9c9a6726ce83752e31d9f1c3db861320170f07da
ArielSimao <arielsimao@outlook.com>
2018-04-09 19:20:52 -0300

    Initial commit

---
+++ NavigationDrawerPopUpMenu2/Login.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NavigationDrawerPopUpMenu2
+{
+    /// <summary>
+    /// Lógica interna para Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            MainWindow jan2 = new MainWindow();
+            jan2.Show();
+        }
+        private void BtnSair(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnSairClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
